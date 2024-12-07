8ff8177258b47e6a64fc3f3c39f215441956b22a
Eliakim Nyawinda <103770049+eleeakim@users.noreply.github.com>
2024-12-07 16:25:48 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
         <Suspense>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
